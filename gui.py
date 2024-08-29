8e97469ff765596746b641625a90f315c35006a0
ZANC0 <111579568+ZANC0@users.noreply.github.com>
2024-08-29 18:42:13 +0100

    Trying to create a selection of uploaded images to select from

--- gui.py
+++ gui.py
@@ -7,6 +7,16 @@ import pydicom as pyd
 
 WIDTH = 1280
 HEIGHT = 720
+# Thinking like when an image is uploaded, it creates a new object that has its own methods to select the image to display on the
+# main page
+class ImageButton(ct.CTkButton):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+class ImageSelection(ct.CTkScrollableFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        
 class App(ct.CTk):
     def __init__(self):
         super().__init__()
@@ -17,44 +27,7 @@ class App(ct.CTk):
                 
         self.default_img = "image-icon.png"
         self.selectedImagePath = "File Path"
-        
-        
-        def setPreviewImage(path=self.default_img):
-            if ".dcm" in path:
-                img = pyd.dcmread(path)
-                img = img.pixel_array
-                img = Image.fromarray(img)
-            else:
-                img = Image.open(path)
-            
-            img = ct.CTkImage(light_image=img, dark_image=img, size=(512,512))
-            self.show_image.configure(image=img, bg_color="black")
-            self.selectedImagePath = path.split("/")[len(path.split("/"))-1]
-            self.file_path_entry.configure(text=self.selectedImagePath)
-
-        def selectImage():
-            try:
-                filename = filedialog.askopenfilename(
-                    initialdir=os.getcwd(),
-                    title = "Upload Image",
-                    filetypes=(("png images", "*.png"),("jpg images", "*.jpg"),("jpeg images", "*.jpeg"),("dcm images","*.dcm"))            
-                )
-                setPreviewImage(filename)
-            except:
-                pass
-
-        def loadIcons():
-            for i in range(3):
-                self.icon = ct.CTkFrame(
-                    self,
-                    width=64,
-                    height=64,
-                    fg_color="white",
-                    border_color="black"
-
-                    
-                )
-                self.icon.grid(row=0,column=0,pady=i*10, padx=0,ipady=0)
+        self.uploaded_images = []
 
         self.mainframe = ct.CTkFrame(
             self,
@@ -62,9 +35,6 @@ class App(ct.CTk):
             width=WIDTH,
             height=HEIGHT
         )
-        self.mainframe.grid(row=0, column=0)
-
-        # self.upload_btn = ct.CTkButton(self,width=100,height=50, text="Upload")
         self.file_path_entry = ct.CTkLabel(
             self,
             text=self.selectedImagePath,
@@ -83,13 +53,55 @@ class App(ct.CTk):
             width=512,
             height=512,
             text="",
-            command=selectImage,
+            command=self.uploadImage
         )
-        loadIcons()
-
-
+        
+        self.selection = ImageSelection(
+            master=self,
+            uploaded_images=self.uploaded_images,
+            width=100,
+            height=200
+        )
+        
+        self.mainframe.grid(row=0, column=0)
         self.file_path_entry.grid(row=0, column=0, padx=0, pady=5, ipady=0, sticky="nw")
         self.show_image.grid(row=0, column=0, padx=0, pady=0, ipady=0,ipadx=0, sticky="e")
+        self.selection.grid(row=0,column=0,padx=0, pady=0, ipady=0,ipadx=0, sticky="w")
+        
+
+
+    def openImage(self,path):
+        if ".dcm" in path:
+            img = pyd.dcmread(path)
+            img = img.pixel_array
+            img = Image.fromarray(img)
+        else:
+            img = Image.open(path)
+        return img
+            
+    def setPreviewImage(self,path="image-icon.png"):
+        img = self.openImage(path)
+        img = ct.CTkImage(light_image=img, dark_image=img, size=(512,512))
+
+        self.uploaded_images.append(path)
+        # self.selectedImagePath = path.split("/")[len(path.split("/"))-1]
+        self.show_image.configure(image=img, bg_color="black")
+        self.file_path_entry.configure(text=self.selectedImagePath)
+
+    def uploadImage(self):
+        try:
+            filename = filedialog.askopenfilename(
+                initialdir=os.getcwd(),
+                title = "Upload Image",
+                filetypes=(("png images", "*.png"),("jpg images", "*.jpg"),("jpeg images", "*.jpeg"),("dcm images","*.dcm"))            
+            )
+            self.setPreviewImage(filename)
+        except:
+            pass
+
+
+
+        
 
 
 
